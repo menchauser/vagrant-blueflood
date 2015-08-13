4fdae80de536a09637646c32248f2f3ecb438db0
Mukhamed Karanashev <menchauser@gmail.com>
2015-08-13 18:02:21 +0400

    Blueflood is not going to be started during provisioning

--- provision-blueflood.sh
+++ provision-blueflood.sh
@@ -55,14 +55,14 @@ cp /vagrant/blueflood.conf /src/blueflood/
 cp /vagrant/blueflood-log4j.properties /src/blueflood/
 
 # Start BlueFlood
-/usr/bin/java \
-        -Dblueflood.config=file:blueflood.conf \
-        -Dlog4j.configuration=file:blueflood-log4j.properties \
-        -Xms1G \
-        -Xmx1G \
-        -Dcom.sun.management.jmxremote.authenticate=false \
-        -Dcom.sun.management.jmxremote.ssl=false \
-        -Djava.rmi.server.hostname=127.0.0.1 \
-        -Dcom.sun.management.jmxremote.port=9180 \
-        -classpath blueflood-all-2.0.0-SNAPSHOT-jar-with-dependencies.jar com.rackspacecloud.blueflood.service.BluefloodServiceStarter
+#/usr/bin/java \
+#        -Dblueflood.config=file:blueflood.conf \
+#        -Dlog4j.configuration=file:blueflood-log4j.properties \
+#        -Xms1G \
+#        -Xmx1G \
+#        -Dcom.sun.management.jmxremote.authenticate=false \
+#        -Dcom.sun.management.jmxremote.ssl=false \
+#        -Djava.rmi.server.hostname=127.0.0.1 \
+#        -Dcom.sun.management.jmxremote.port=9180 \
+#        -classpath blueflood-all-2.0.0-SNAPSHOT-jar-with-dependencies.jar com.rackspacecloud.blueflood.service.BluefloodServiceStarter
 
